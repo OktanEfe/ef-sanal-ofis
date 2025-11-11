498834c9b2d9f4a94b4a85b194bb58ad99c924c1
OktanEfe <oktanefe1589@outlook.com>
2025-11-11 06:30:28 +0300

    remove validation,Add sendmail.js, enable Brevo mail.js handler

--- js/mail.js
+++ js/mail.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
       };
   
       try {
-        const res = await fetch("/.netlify/functions/sendMail", {
+        const res = await fetch("/netlify/functions/sendMail", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(data),
